42a1a44d4eba93e0b34b4701c466ba8880da1815
earghand <eliasarghand@gmail.com>
2021-06-18 14:58:25 -0700

    fix

--- Backend/routes/individual.js
+++ Backend/routes/individual.js
@@ -4,15 +4,15 @@ const fetch = require('node-fetch');
 const fs = require('fs')
 // const jsonFile = require('../assets/items.json');
 const { json } = require('body-parser');
-let rawdata = ""
-let features = ""
-try {
-    rawdata = fs.readFileSync('items.json')
-    features = JSON.parse(rawdata); 
-} catch (error) {
-    console.log(error);   
-}
+
+let rawdata = fs.readFileSync('items.json')
+let features = JSON.parse(rawdata);
+
 router.get('/', async(req,res)=>{
+    const params = {
+        ids: req.query.id,
+    }
+    console.log(params.ids);
     res.send(features)
 });
 
--- Backend/server.js
+++ Backend/server.js
@@ -3,12 +3,14 @@ const morgan = require("morgan")
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const featured = require('./routes/featured')
+const individual = require('./routes/individual')
 
 const app = express()
 
 app.use(morgan("tiny"))
 app.use(cors())
 app.use('/api/featured', featured)
+app.use('/api/individual', individual)
 app.get('/', (req, res) => res.send("store"));
 app.get((req, res, next) => {
   res.status(200).json({ping: "pong"})
